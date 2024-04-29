e3257566c95921075e98ceb3b4e21b23924ba112
Carl Ern Banua <phantomhack21.carl@gmail.com>
2024-04-30 07:22:57 +0800

    mod 04-30-2024
    mod 04-30-2024

--- utils/global-data.js
+++ utils/global-data.js
@@ -1,13 +1,13 @@
 export const getGlobalData = () => {
   const name = process.env.BLOG_NAME
     ? decodeURI(process.env.BLOG_NAME)
-    : 'Jay Doe';
+    : 'Carl Ern Banua';
   const blogTitle = process.env.BLOG_TITLE
     ? decodeURI(process.env.BLOG_TITLE)
     : 'Next.js Blog Theme';
   const footerText = process.env.BLOG_FOOTER_TEXT
     ? decodeURI(process.env.BLOG_FOOTER_TEXT)
-    : 'All rights reserved.';
+    : 'All rights reserved 2024.';
 
   return {
     name,
