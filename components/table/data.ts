2bdfcb7ff1bae30901e91589c44119bacfc0e4ee
Carl Ern Banua <phantomhack21.carl@gmail.com>
2024-06-08 01:03:41 +0800

    q
    q

--- components/table/data.ts
+++ components/table/data.ts
@@ -2,8 +2,7 @@ export const columns = [
    {name: 'NAME', uid: 'name'},
    {name: 'ROLE', uid: 'role'},
    {name: 'STATUS', uid: 'status'},
-   {name: 'ACTIONS', uid: 'actions'},
-   {name: 'SCHOOL', uid: 'school'},
+   {name: 'ACTIONS', uid: 'actions'},   
 ];
 
 export const columnseducationalbackground = [
