fd7134643494a9595c137698bd58486feeaa3cb8
Carl Ern Banua <phantomhack21.carl@gmail.com>
2024-08-26 23:11:18 +0800

    update
    update

--- components/educationalbackground/educationbackgrounddata.ts
+++ components/educationalbackground/educationbackgrounddata.ts
@@ -8,11 +8,19 @@ export const columnseducationalbackground = [
  export const educationalbackgrounds = [
     {
        id: 1,
-       name: '',
-       grade: 'elementary',
+       name: 'Bong-ao Elementary School',
+       grade: 'Elementary',
        location: 'Bong-ao, Valencia Negros Oriental',
        yearstarted: '2018',
        yearended: '2018'
     },
+    {
+      id: 2,
+      name: 'Valencian National High Schrool',
+      grade: 'HighSchool',
+      location: 'Bong-ao, Valencia Negros Oriental',
+      yearstarted: '2018',
+      yearended: '2018'
+   },
  ];
  
